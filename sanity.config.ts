3eec4f9bf7e6c25a9164cadf1028527bb8e9eb21
donecorleone <88971777+donecorleone@users.noreply.github.com>
2024-10-16 17:23:00 +0200

    xx
    xx

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,34 +1,16 @@
-// Different environments use different variables
-const projectId =
-  import.meta.env.PUBLIC_SANITY_STUDIO_PROJECT_ID! ||
-  import.meta.env.PUBLIC_SANITY_PROJECT_ID!;
-const dataset =
-  import.meta.env.PUBLIC_SANITY_STUDIO_DATASET! ||
-  import.meta.env.PUBLIC_SANITY_DATASET!;
-
-// Feel free to remove this check if you don't need it
-if (!projectId || !dataset) {
-  throw new Error(
-    `Missing environment variable(s). Check if named correctly in .env file.\n\nShould be:\nPUBLIC_SANITY_STUDIO_PROJECT_ID=${projectId}\nPUBLIC_SANITY_STUDIO_DATASET=${dataset}\n\nAvailable environment variables:\n${JSON.stringify(
-      import.meta.env,
-      null,
-      2
-    )}`
-  );
-}
-
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
-import { visionTool } from "@sanity/vision";
-import { schemaTypes } from "./schema";
+import { schemaTypes } from "./src/sanity/schema";
+import { presentationTool } from "sanity/presentation";
 
 export default defineConfig({
-  name: "project-name",
-  title: "Project Name",
-  projectId,
-  dataset,
-  plugins: [structureTool(), visionTool()],
-  schema: {
-    types: schemaTypes,
-  },
+  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
+  dataset: import.meta.env.PUBLIC_SANITY_DATASET,
+  plugins: [
+    structureTool(),
+    presentationTool({
+      previewUrl: location.origin,
+    }),
+  ],
+  schemaTypes,
 });
---
+++ src/sanity/schema/index.ts
@@ -0,0 +1,6 @@
+import blockContent from './blockContent'
+import post from './post'
+import category from './category';
+import author from './author';
+
+export const schemaTypes = [post, blockContent, category, author ]
